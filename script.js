aa4847d3bc76717001d9dc1eacc515f637728a80
dachi <dachi.laferashvili@gmail.com>
2024-11-29 23:55:31 +0400

    created mobile navigation

---
+++ script.js
@@ -0,0 +1,8 @@
+// Making mobile navigation work
+
+const btnNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+
+btnNav.addEventListener("click", () => {
+  header.classList.toggle("nav-open");
+});
