60f5c8709824f972c10337b245077d1826d47f90
dachi <dachi.laferashvili@gmail.com>
2024-12-03 00:57:12 +0400

    implemented smooth scrolling

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+///////////////////////////////////////////////
 // Making mobile navigation work
 
 const btnNav = document.querySelector(".btn-mobile-nav");
@@ -5,4 +6,35 @@ const header = document.querySelector(".header");
 
 btnNav.addEventListener("click", () => {
   header.classList.toggle("nav-open");
+});
+
+/////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+
+    // Scroll back to the top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      header.classList.remove("nav-open");
+    }
+  });
 });
